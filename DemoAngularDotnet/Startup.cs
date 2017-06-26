239142a7525b93d01cd0cdfb3b4584fa56ba1a8b
PoyChang <poypost@gmail.com>
2017-06-26 22:59:40 +0800

    判斷 Request 的目的，看是要存取網頁或是呼叫 API 等動作

--- DemoAngularDotnet/Startup.cs
+++ DemoAngularDotnet/Startup.cs
@@ -37,6 +37,21 @@ namespace DemoAngularDotnet
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
+            app.Use(async (context, next) =>
+            {
+                await next();
+
+                // 判斷是否是要存取網頁，而不是發送 API 需求
+                if (context.Response.StatusCode == 404 &&                       // 該資源不存在
+                    !System.IO.Path.HasExtension(context.Request.Path.Value) && // 網址最後沒有帶副檔名
+                    !context.Request.Path.Value.StartsWith("/api"))             // 網址不是 /api 開頭
+                {
+                    context.Request.Path = "/index.html";                       // 將網址改成 /index.html
+                    context.Response.StatusCode = 200;                          // 並將 HTTP 狀態碼修改為 200 成功
+                    await next();
+                }
+            });
+
             app.UseDefaultFiles();
             app.UseStaticFiles();
 
